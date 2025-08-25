89fa7494fef7b5a68da290d1defbcf936d5f8be7
Vitor Zary <hugozary@gmail.com>
2025-08-25 20:00:10 -0300

    criando testes para o cadastro

---
+++ cypress/e2e/cadastro-correto.cy.js
@@ -0,0 +1,15 @@
+describe('Página de cadastro', () => {
+  beforeEach(() => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app');
+    cy.get('[data-test="register-button"]').click();
+  })
+  
+  it('Deve preencher os campos do formulário corretamente para cadastrar um novo usuário.', () => {
+    cy.get('[data-test="input-name"]').type("Deodoro Silva");
+    cy.get('[data-test="input-email"]').type("deodorosilva@email.com");
+    cy.get('[data-test="input-password"]').type("Senha123");
+    cy.get('[data-test="input-confirm-password"]').type("Senha123");
+    cy.get('[data-test="submit-button"]').click();
+    
+  })
+})
