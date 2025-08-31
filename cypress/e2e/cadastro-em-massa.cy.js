b79a8d160bc9d48ab8d3cc13accc089d8959d7b3
Vitor Zary <hugozary@gmail.com>
2025-08-31 11:52:49 -0300

    Alterando o arquivo cadastro-em-massa.cy.js para gerar valores aleatorios para e-mail e senha.

--- cypress/e2e/cadastro-em-massa.cy.js
+++ cypress/e2e/cadastro-em-massa.cy.js
@@ -1,15 +1,15 @@
-import { usuarios } from '../fixtures/usuarios.json';
-
 describe('Página de cadastro', () => {
   beforeEach(() => {
     cy.visit('https://adopet-frontend-cypress.vercel.app');
     cy.get('[data-test="register-button"]').click();
   })
 
-  usuarios.forEach(usuario => {
+  for (let i = 0; i < 5; i++) {
     it('Deve preencher os campos e cadastra-los corretamente', () => {
-        cy.cadastrar(usuario.name, usuario.email, usuario.password);
+        let uuid = self.crypto.randomUUID();
+        cy.cadastrar('teste cadastro', `${uuid}@email.com`, `S${Math.random().toString(36).slice(-10)}`);
+        cy.url().should('eq', 'https://adopet-frontend-cypress.vercel.app/login');
     })
-  })
-
+  }
+    
 })
