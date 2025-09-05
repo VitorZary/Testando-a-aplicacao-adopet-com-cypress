e543f87bce0df24a40ddb1cb3c8e2ccf975568c7
Vitor Zary <hugozary@gmail.com>
2025-09-05 10:52:26 -0300

    Adicionando testes de API para testar a funcionalidade de mensagens do sistema.

---
+++ cypress/e2e/api-mensagens-cypress.cy.js
@@ -0,0 +1,14 @@
+describe('Api mensagens', ()=> {
+
+    it('Mensagens da API', () => {
+        cy.request({
+            method: 'GET',
+            url: 'https://adopet-api-i8qu.onrender.com/mensagem/19306bda-7fbe-465b-a06b-c3de1248bbc2',
+            headers: Cypress.env()
+        }).then((res) => {
+            expect(res.status).to.be.equal(200)
+            expect(res.body).is.not.empty
+            expect(res.body).to.have.property('msg')
+        })
+    })
+})
